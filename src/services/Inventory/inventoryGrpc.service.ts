48a3d8694e67cd04a230ed9e177f5438696434a1
Peerapat Toprasert <peerapat.toprasert@gmail.com>
2023-07-25 21:49:24 +0700

    feat:grpc

--- src/services/Inventory/inventoryGrpc.service.ts
+++ src/services/Inventory/inventoryGrpc.service.ts
@@ -1,7 +1,16 @@
-import { Controller, Injectable, Logger, OnModuleInit } from '@nestjs/common'
+import { Metadata } from '@grpc/grpc-js'
+import { GrpcAuthGuard } from '@march/core'
+import {
+  Controller,
+  Injectable,
+  Logger,
+  OnModuleInit,
+  UseGuards,
+} from '@nestjs/common'
 import { GrpcMethod } from '@nestjs/microservices'
 import { PrismaService } from 'src/common/prisma/prisma.service'
 import { inventory } from 'src/types/grpc/proto/inventory'
+// import { GrpcAuthGuard } from './grpc.guard'
 
 @Injectable()
 @Controller()
@@ -11,17 +20,17 @@ export class InventoryGrpcService implements OnModuleInit {
   constructor(private readonly repos: PrismaService) {}
   onModuleInit() {}
 
+  @UseGuards(GrpcAuthGuard)
   @GrpcMethod(InventoryGrpcService.name, 'tester')
-  // @UseGuards(GrpcAuthGuard)
-  async tester(): Promise<inventory.InventoryNameList> {
-    console.log('grpc pas2s')
+  async tester(data, metadata: Metadata): Promise<inventory.InventoryNameList> {
+    console.log('grpc pas2s', { metadata: metadata.getMap() })
     return {
       inventoryName: [
         {
           id: '123',
           name: '123',
         },
-        {
+        { 
           id: '1234',
           name: '1234',
         },
